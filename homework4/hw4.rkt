06be7a17d454c81d978163b7c28e5b97f4e0eeff
Lifan Sun <201250181@smail.nju.edu.cn>
2023-11-10 15:59:29 +0800

    hw4 starter code

---
+++ homework4/hw4.rkt
@@ -0,0 +1,17 @@
+
+#lang racket
+
+;; This line exports all of your defined functions,
+;; so you can call them in hw4tests.rkt and so we can
+;; call them in our tests.
+;; Don't remove or comment out this line!
+(provide (all-defined-out)) 
+
+;; Implement your code below
+
+;; #1
+(define (sequence spacing low high)
+  (error "need to implement 'sequence'"))
+
+;; keep going with the rest of the problems below
+
