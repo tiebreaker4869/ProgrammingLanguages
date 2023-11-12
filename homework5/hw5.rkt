ade10cb5f60c0c558be4d6d778ef63f5621f5129
Lifan Sun <201250181@smail.nju.edu.cn>
2023-11-12 23:12:42 +0800

    implemented problem 1 and 2

--- homework5/hw5.rkt
+++ homework5/hw5.rkt
@@ -25,6 +25,16 @@
 
 ;; CHANGE (put your solutions here)
 
+(define (racketlist->mupllist xs)
+  (cond
+    [(null? xs) (munit)]
+    [#t (apair (car xs) (racketlist->mupllist (cdr xs)))]))
+
+(define (mupllist->racketlist xs)
+  (cond
+    [(munit? xs) null]
+    [#t (cons (apair-e1 xs) (mupllist->racketlist (apair-e2 xs)))]))
+
 ;; Problem 2
 
 ;; lookup a variable in an environment
@@ -49,6 +59,48 @@
                (int (+ (int-num v1) 
                        (int-num v2)))
                (error "MUPL addition applied to non-number")))]
+        [(isgreater? e)
+         (let ([v1 (eval-under-env (isgreater-e1 e) env)]
+               [v2 (eval-under-env (isgreater-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (if (> (int-num v1) (int-num v2))
+                   (int 1)
+                   (int 0))
+               (error "MUPL isgreater applied to non-number")))]
+        [(ifnz? e) (let ([v1 (eval-under-env (ifnz-e1 e) env)])
+                     (if (int? v1)
+                         (if (= 0 (int-num v1))
+                             (eval-under-env (ifnz-e3 e) env)
+                             (eval-under-env (ifnz-e2 e) env))
+                         (error "MUPL ifnz condition applied to non-number")))]
+        [(apair? e) (let ([v1 (eval-under-env (apair-e1 e) env)]
+                          [v2 (eval-under-env (apair-e2 e) env)])
+                      (apair v1 v2))]
+        [(first? e) (let ([v1 (eval-under-env (first-e e) env)])
+                      (if (apair? v1)
+                          (apair-e1 v1)
+                          (error "MUPL first applied to non-pair")))]
+        [(second? e) (let ([v1 (eval-under-env (second-e e) env)])
+                       (if (apair? v1)
+                           (apair-e2 v1)
+                           (error "MUPL second applied to non-pair")))]
+        [(munit? e) e]
+        [(int? e) e]
+        [(ismunit? e) (if (munit? (ismunit-e e))
+                          (int 1)
+                          (int 0))]
+        [(fun? e) (closure env e)]
+        [(mlet? e) (let ([v1 (eval-under-env (mlet-e e) env)])
+                     (eval-under-env (mlet-body e) (cons (cons (mlet-var e) v1) env)))]
+        [(call? e) (let ([v1 (eval-under-env (call-funexp e) env)]
+                         [v2 (eval-under-env (call-actual e) env)])
+                     (if (closure? v1)
+                         (let ([extend-arg-env (cons (cons (fun-formal (closure-fun v1)) v2) (closure-env v1))])
+                           (if (null? (fun-nameopt (closure-fun v1)))
+                               (eval-under-env (fun-body (closure-fun v1)) extend-arg-env)
+                               (eval-under-env (fun-body (closure-fun v1)) (cons (cons (fun-nameopt (closure-fun v1)) v1) extend-arg-env))))
+                         (error "MUPL call applied to non-closure")))]
         ;; CHANGE add more cases here
         [#t (error (format "bad MUPL expression: ~v" e))]))
 
